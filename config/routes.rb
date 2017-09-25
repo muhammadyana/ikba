6e7da4cb80ba52cb7dc225b641619e7538bb74b1
muhammadyana <muhammadyanaa@gmail.com>
2017-09-25 18:40:45 +0700

    Add devise controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
   get 'static_page/home'
   root 'static_page#home'
 
