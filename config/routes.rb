e680e4ceb8fe1e7cdd2e9f9ddf141e02d6055e8a
muhammadyana <muhammadyanaa@gmail.com>
2017-10-06 22:57:23 +0700

    Clean routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,5 @@ Rails.application.routes.draw do
   end
   get 'static_page/home'
   root 'static_page#home'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
