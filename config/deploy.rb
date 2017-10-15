8a15fb4981ba357b1cb4ae5bb0c89e85f5b3322e
muhammadyana <muhammadyanaa@gmail.com>
2017-10-15 18:54:21 +0700

    Add capistrano setting

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid only for current version of Capistrano
 lock "3.9.1"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "ikba"
+set :repo_url, "git@github.com:muhammadyana/ikba.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
